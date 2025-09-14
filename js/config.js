80eebf6b4c6b14ba7268599225862bf9ef03eabb
onzefty <aurelien.bouillon11@gmail.com>
2025-09-14 19:44:22 +0200

    header updated

--- js/config.js
+++ js/config.js
@@ -3,28 +3,28 @@ export default {
     projects: [
         {
             title: "IFCAM",
-            description: "Module de formation sur les Risques Environnementaux",
+            description: "Module de formation sur les Risques Environnementaux.",
             tools: ["HTML5","CSS3","JavaScript","Adobe Photoshop","Animations Customs","Sounds"],
             url: "./assets/projects/ifcam/risques-n2/risques-n2/index.html",
             image: "./assets/imgs/projects/ifcam.webp"
         },
         {
             title: "Rugby World Cup 2023",
-            description: "Module de formation sur la Coupe du Monde de Rugby 2023",
+            description: "Module de formation pour les Volontaires, dans le cadre de la Coupe du Monde de Rugby 2023.",
             tools: ["HTML5","CSS3","JavaScript","Adobe Photoshop","Animations Customs"],
             url: "./assets/projects/ifcam/rugby/index.html",
             image: "./assets/imgs/projects/rugby.webp"
         },
         {
             title: "Hermès Maison",
-            description: "Module de formation sur les 5 règles d'Hermès pour la disposition des articles sur étagère",
+            description: "Module de formation sur les 5 règles d'Hermès pour la disposition des articles sur étagère.",
             tools: ["HTML5","CSS3","JavaScript","Adobe Photoshop","Animations Customs","Sounds"],
             url: "./assets/projects/hermes-maison/module/index.html",
             image: "./assets/imgs/projects/rugby.webp"
         },
         {
             title: "Renault",
-            description: "Module de formation pour le Groupe Renault",
+            description: "Module de formation pour le Groupe Renault.",
             tools: ["HTML5","CSS3","JavaScript","Adobe Photoshop","Animations Customs","Videos","IA"],
             url: "./assets/projects/renault/M3/index.html",
             image: "./assets/imgs/projects/rugby.webp"
